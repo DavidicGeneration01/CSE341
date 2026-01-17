09e0d8c401713393745692020f985c3457c47eea
DavidicGeneration01 <dalade@byupathway.edu >
2026-01-17 20:28:27 +0100

    Project 2 - Contact

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
+//#swagger.tags=['Hello World']
 router.get('/', (req, res) => { res.send('Hello World!  ');});
 
 router.use('/users', require('./users'));
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-require('dotenv').config();
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.use('/', require('./routes'));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
 
+app.use('/', require('./routes/index'));
 
 mongodb.initDb((err) => {
   if (err) {
